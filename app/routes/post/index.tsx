8f92664ae7691738b4d9f6c30c7fc74b4ccff449
moon <his.methods@gmail.com>
2022-10-21 00:16:55 +0900

    npm 에서 yarn으로 패키지매니저 변경

--- app/routes/post/index.tsx
+++ app/routes/post/index.tsx
@@ -32,8 +32,8 @@ type LoaderData = {
 
 export default function PostIndex() {
   return (
-    <div className="container mx-auto max-w-3xl">
-      <h1 className="text-7xl mb-16">블로그</h1>
+    <div className="container max-w-3xl mx-auto">
+      <h1 className="mb-16 text-7xl">블로그</h1>
       <ArticleList />
     </div>
   );
@@ -62,7 +62,7 @@ function PostListItem({ post }: { post: PostInfo }) {
           <span className="text-sm">{postDate}</span>
         </div>
         <div className="flex-1">
-          <h3 className="text-2xl leading-tight mt-0">{post.title}</h3>
+          <h3 className="mt-0 text-2xl leading-tight">{post.title}</h3>
         </div>
       </div>
     </Link>
@@ -72,14 +72,14 @@ function PostListItem({ post }: { post: PostInfo }) {
 function PostCard({ linkTo, title, content, imageUrl }: { linkTo: string; title: string; content: string; imageUrl: string | null }) {
   return (
     <Link to={linkTo} prefetch="intent">
-      <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
         {imageUrl && <img className="rounded-t-lg" src={imageUrl} alt="" />}
         <div className="p-5">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{content}</p>
-          <span className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <span className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Read more
-            <svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path
                 fillRule="evenodd"
                 d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
